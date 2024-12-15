637b8f4eab96542b3122543a24d478acd13189dc
Mohammedar12 <alikkar87@gmail.com>
2024-12-15 12:22:45 +0300

    ad

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,11 @@ const nextConfig = {
     domains: ["res.cloudinary.com", "cdn-ray.zain.sa"],
   },
   env: {
-    NEXT_PUBLIC_BASE_URL: "http://localhost:5000",
+    NEXT_PUBLIC_OPEN_AI_SEC_KEY:
+      "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",
+    NEXT_PUBLIC_BASE_URL: "http://165.227.119.249",
+    NEXT_PUBLIC_CLIENT_URL: "http://165.227.119.249:3000",
+    NEXT_PUBLIC_IFRAME_URL: "165.227.119.249:3000",
   },
 };
 
