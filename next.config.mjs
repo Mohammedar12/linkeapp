0e52febb76c6863f89996f584db8e14e0fef0308
Mohammedar12 <alikkar87@gmail.com>
2024-12-16 01:02:10 +0300

    add yarn

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,6 @@ const nextConfig = {
     NEXT_PUBLIC_CLIENT_URL: "http://165.227.119.249:3000",
     NEXT_PUBLIC_IFRAME_URL: "165.227.119.249:3000",
   },
-  experimental: {
-    cpus: 1,
-  },
 };
 
 export default nextConfig;
