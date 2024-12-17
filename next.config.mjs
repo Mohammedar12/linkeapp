e2fbe6e0c6fd07395118a65f3b3d19af3896b035
Mohammedar12 <alikkar87@gmail.com>
2024-12-17 12:00:39 +0300

    fix vars

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,6 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com", "cdn-ray.zain.sa"],
   },
-  env: {
-    NEXT_PUBLIC_OPEN_AI_SEC_KEY:
-      "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",
-    NEXT_PUBLIC_BASE_URL: "http://165.227.119.249",
-    NEXT_PUBLIC_CLIENT_URL: "http://165.227.119.249:3000",
-    NEXT_PUBLIC_IFRAME_URL: "165.227.119.249:3000",
-  },
 };
 
 export default nextConfig;
